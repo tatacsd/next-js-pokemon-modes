8885ad7255362125edc03716cf77395e462bbb32
thays <thayscasado@hotmail.com>
2022-04-28 18:32:57 -0700

    mobile reponsive

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,19 @@ export default function Home() {
       <div className={styles.header}>
         <h1>Pokemon List</h1>
       </div>
+      {/* Add pagination
+      <div className={styles.paginationContainer}>
+      <a href="#">&laquo;</a>
+      <a href="#">1</a>
+      <a href="#" class="active">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">&raquo;</a>
+      </div> */}
+
+
       <div className={styles.grid}>
         {pokemons.map((pokemon) => (
           <div className={styles.card} key={pokemon.id}>
